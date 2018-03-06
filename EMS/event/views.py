38e037ef47ddef63deff4f42ea37184cd27285c8
hafeezali <hafeez.ali5@gmail.com>
2018-03-06 14:07:43 +0530

    Added base.html & home.html

--- EMS/event/views.py
+++ EMS/event/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from .models import Event, Profile, Ticket
+from django.db.models import Q
 
-def index(request):
+
+def home(request):
     events = Event.objects.all()
-
+    query = request.GET.get("query")
+    if query:
+        events = events.filter(
+            Q(name__icontains=query) |
+            Q(manager__first_name__icontains=query) |
+            Q(manager__last_name__icontains=query)
+        ).distinct()
+        return render(request, 'event/home.html', {
+            'events': events,
+        })
+    else:
+        return render(request, 'event/home.html', {
+            'events': events,
+        })
